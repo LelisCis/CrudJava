52b28690899892e574af66acf3793543a107a51f
LelisCis <89097785+LelisCis@users.noreply.github.com>
2021-12-16 10:56:55 -0300

    Ultimos cambios en CRUD
    CRUD casi listo con cambios en css

---
+++ src/main/java/modelo/ClientesDAO.java
@@ -0,0 +1,132 @@
+package modelo;
+
+import config.Conexion;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientesDAO {
+    Connection conexion;
+    public ClientesDAO(){
+        Conexion con = new Conexion();
+        conexion = con.getConection();
+    }
+    public List<Clientes> listarClientes(){
+       PreparedStatement ps;
+       ResultSet rs;
+       List<Clientes> lista = new ArrayList<>();
+       /*List es abstracta*/
+       try{
+           ps = conexion.prepareStatement("SELECT * FROM participantes");
+           rs = ps.executeQuery();
+           
+           while(rs.next()){
+               int id = rs.getInt("id");
+               String nombre = rs.getString("nombre");
+               String apellido = rs.getString("apellido"); 
+               String email = rs.getString("email");
+               int telefono = rs.getInt("telefono");
+               Clientes clientes = new Clientes(id, nombre, apellido,
+               email, telefono);
+               lista.add(clientes);
+           }
+           return lista;
+           
+       }catch(SQLException e){
+           System.out.println(e.toString());
+           return null;
+       }
+    }
+    public Clientes mostrarCliente(int _id){
+       PreparedStatement ps;
+       ResultSet rs;
+       Clientes cliente = null;
+       
+       try{
+            ps = conexion.prepareStatement("SELECT * FROM "
+                    + "participantes WHERE id=?");
+            ps.setInt(1,_id);
+           rs = ps.executeQuery();
+           while(rs.next()){
+               int id = rs.getInt("id");
+               String nombre = rs.getString("nombre");
+               String apellido = rs.getString("apellido"); 
+               String email = rs.getString("email");
+               int telefono = rs.getInt("telefono");
+               
+               cliente = new Clientes(id,nombre,apellido,
+               email, telefono);
+           } return cliente;
+           
+       }catch(SQLException e){
+           System.out.println(e.toString());
+           return null;
+       }
+       
+    }
+    public boolean insertarCliente(Clientes cliente){
+        PreparedStatement ps;
+        try{
+             ps = conexion.prepareStatement("INSERT INTO "
+                     + "participantes"
+                     + "(nombre,apellido,email,telefono)"
+                     + "VALUES (?,?,?,?)");
+             ps.setString(1,cliente.getNombre());
+             ps.setString(2,cliente.getApellido());
+             ps.setString(3,cliente.getEmail());
+             ps.setInt(4, cliente.getTelefono());
+             ps.execute();
+             return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+                
+    }
+    public boolean actualizarCliente(Clientes cliente){
+        PreparedStatement ps;
+        try{
+            ps = conexion.prepareStatement("UPDATE participantes SET nombre=?, apellido=?, email=?, telefono=? where id=?");
+                       
+             
+             ps.setString(1,cliente.getNombre());
+             ps.setString(2,cliente.getApellido());
+             ps.setString(3,cliente.getEmail());
+             ps.setInt(4, cliente.getTelefono());
+             ps.setInt(5, cliente.getId());
+             ps.execute();
+             return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+    public boolean eliminarCliente(int _id){
+        PreparedStatement ps;
+          try{
+            ps = conexion.prepareStatement("DELETE FROM "
+                    + "participantes WHERE id=?");
+            ps.setInt(1,_id);
+            ps.execute();
+            return true;
+          }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+     public boolean ingresarUsuario(String email, String clave){
+        PreparedStatement ps;
+        ResultSet rs;
+        try{
+            ps= conexion.prepareStatement("SELECT * FROM usuarios WHERE email=?");
+            ps.setString(1,email);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                return email.equals(rs.getString("email"))&& clave.equals(rs.getString("password"));
+            }return false;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+}
